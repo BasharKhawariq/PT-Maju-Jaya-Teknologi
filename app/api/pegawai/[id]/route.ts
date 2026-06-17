3024d587f2db68870d2a0dab3db2d5580fe6fb29
BasharKhawariq <bhasarkhawariq@gmail.com>
2026-06-17 12:52:38 +0700

    upd : detail pegawai

--- app/api/pegawai/[id]/route.ts
+++ app/api/pegawai/[id]/route.ts
@@ -1,33 +1,59 @@
-import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  const data = await prisma.pegawai.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      jabatan: true,
+    },
+  });
+
+  return NextResponse.json(data);
+}
 
 export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await params;
   const body = await req.json();
 
-  const data = await prisma.jabatan.update({
+  const data = await prisma.pegawai.update({
     where: {
-      id: Number(params.id),
+      id: Number(id),
     },
-    data: body,
+    data: {
+      nama: body.nama,
+      gelar: body.gelar,
+      jabatanId: body.jabatanId,
+    },
   });
 
   return NextResponse.json(data);
 }
 
 export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  await prisma.jabatan.delete({
+  const { id } = await params;
+
+  await prisma.pegawai.delete({
     where: {
-      id: Number(params.id),
+      id: Number(id),
     },
   });
 
   return NextResponse.json({
-    message: "Deleted",
+    success: true,
+    message: "Pegawai berhasil dihapus",
   });
 }
